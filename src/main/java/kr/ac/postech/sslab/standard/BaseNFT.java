06c1a3a701c931cd8f07667edb1b8aaf45e6223c
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-07 01:30:28 +0900

    specify dedicated exceptions in BaseNFT

--- src/main/java/kr/ac/postech/sslab/standard/BaseNFT.java
+++ src/main/java/kr/ac/postech/sslab/standard/BaseNFT.java
@@ -1,23 +1,25 @@
 package kr.ac.postech.sslab.standard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.ac.postech.sslab.nft.NFT;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
 public class BaseNFT {
-    public static boolean mint(ChaincodeStub stub, BigInteger tokenId, String owner) throws Exception {
+    public static boolean mint(ChaincodeStub stub, BigInteger tokenId, String owner) throws JsonProcessingException {
         NFT nft = new NFT();
         String type = "base";
         return nft.mint(stub, tokenId, type, owner, null, null);
     }
 
-    public static boolean burn(ChaincodeStub stub, BigInteger tokenId) throws Exception {
+    public static boolean burn(ChaincodeStub stub, BigInteger tokenId) throws IOException {
         NFT nft = NFT.read(stub, tokenId);
         return nft.burn(stub, tokenId);
     }
 
-    public static String getType(ChaincodeStub stub, BigInteger tokenId) throws Exception {
+    public static String getType(ChaincodeStub stub, BigInteger tokenId) throws IOException {
         NFT nft = NFT.read(stub, tokenId);
         return nft.getType();
     }
