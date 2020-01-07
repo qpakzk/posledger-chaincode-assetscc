20357e60ab296fc311751b9badd79c9e6a162829
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-07 19:33:20 +0900

    create class DataTypeConversion for data type conversion

--- src/main/java/kr/ac/postech/sslab/extension/XType.java
+++ src/main/java/kr/ac/postech/sslab/extension/XType.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.ac.postech.sslab.main.CustomChaincodeBase;
 import kr.ac.postech.sslab.structure.TokenTypes;
+import kr.ac.postech.sslab.util.DataTypeConversion;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeStub;
@@ -13,9 +14,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.*;
 
-import static kr.ac.postech.sslab.constant.DataType.*;
 import static kr.ac.postech.sslab.constant.Key.TOKEN_TYPES;
-import static kr.ac.postech.sslab.constant.Message.NO_DATA_TYPE_MESSAGE;
 
 public class XType extends CustomChaincodeBase {
     private static final Log LOG = LogFactory.getLog(XType.class);
@@ -63,7 +62,7 @@ public class XType extends CustomChaincodeBase {
                         return false;
                     }
 
-                    Object value = strToDataType(entry.getValue().get(0), entry.getValue().get(1));
+                    Object value = DataTypeConversion.strToDataType(entry.getValue().get(0), entry.getValue().get(1));
                     if (value == null) {
                         return false;
                     }
@@ -104,114 +103,7 @@ public class XType extends CustomChaincodeBase {
         return true;
     }
 
-    private static Object strToDataType(String dataType, String value) {
-        List<String> strings;
-
-        switch (dataType) {
-            case INTEGER:
-                return Integer.parseInt(value);
-
-            case BIG_INTEGER:
-                return new BigInteger(value);
-
-            case DOUBLE:
-                return Double.parseDouble(value);
-
-            case BYTE:
-                return Byte.parseByte(value);
-
-            case STRING:
-                return value;
-
-            case BOOLEAN:
-                return Boolean.parseBoolean(value);
-
-            case LIST_INTEGER:
-                List<Integer> integers = new ArrayList<>();
-                if (value == null) {
-                    return integers;
-                }
-
-                strings = toList(value);
-                for (String string : strings) {
-                    integers.add(Integer.parseInt(string));
-                }
-
-                return integers;
-
-            case LIST_BIG_INTEGER:
-                List<BigInteger> bigIntegers = new ArrayList<>();
-                if (value == null) {
-                    return bigIntegers;
-                }
-
-                strings = toList(value);
-                for (String string : strings) {
-                    bigIntegers.add(new BigInteger(string));
-                }
-
-                return bigIntegers;
-
-            case LIST_DOUBLE:
-                List<Double> doubles = new ArrayList<>();
-                if (value == null) {
-                    return doubles;
-                }
-
-                strings = toList(value);
-                for (String string : strings) {
-                    doubles.add(Double.parseDouble(string));
-                }
-
-                return doubles;
-
-            case LIST_BYTE:
-                List<Byte> bytes = new ArrayList<>();
-                if (value == null) {
-                    return bytes;
-                }
-
-                strings = toList(value);
-                for (String string : strings) {
-                    bytes.add(Byte.parseByte(string));
-                }
-
-                return bytes;
-
-            case LIST_STRING:
-                strings = new ArrayList<>();
-                if (value == null) {
-                    return strings;
-                }
-
-                strings = toList(value);
-                return strings;
-
-            case LIST_BOOLEAN:
-                List<Boolean> booleans = new ArrayList<>();
-                if (value == null) {
-                    return booleans;
-                }
-
-                strings = toList(value);
-                for (String string : strings) {
-                    booleans.add(Boolean.parseBoolean(string));
-                }
-
-                return booleans;
-
-            default:
-                LOG.error(NO_DATA_TYPE_MESSAGE);
-                return null;
-        }
-
-    }
-
     private static String toJSONString(Map<String, Map<String, List<String>>> map) throws JsonProcessingException {
         return mapper.writeValueAsString(map);
-    }
-
-    private static List<String> toList(String value) {
-        return Arrays.asList(value.substring(1, value.length() - 1).split(", "));
     }
 }
---
+++ src/main/java/kr/ac/postech/sslab/util/DataTypeConversion.java
@@ -0,0 +1,145 @@
+package kr.ac.postech.sslab.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static kr.ac.postech.sslab.constant.DataType.*;
+import static kr.ac.postech.sslab.constant.DataType.LIST_BOOLEAN;
+import static kr.ac.postech.sslab.constant.Message.NO_DATA_TYPE_MESSAGE;
+
+public class DataTypeConversion {
+    private static final Log LOG = LogFactory.getLog(DataTypeConversion.class);
+
+    public static Object strToDataType(String dataType, String value) {
+        switch (dataType) {
+            case INTEGER:
+                return Integer.parseInt(value);
+
+            case BIG_INTEGER:
+                return new BigInteger(value);
+
+            case DOUBLE:
+                return Double.parseDouble(value);
+
+            case BYTE:
+                return Byte.parseByte(value);
+
+            case STRING:
+                return value;
+
+            case BOOLEAN:
+                return Boolean.parseBoolean(value);
+
+            case LIST_INTEGER:
+                return toListInteger(value);
+
+            case LIST_BIG_INTEGER:
+                return toListBigInteger(value);
+
+            case LIST_DOUBLE:
+                return toListDouble(value);
+
+            case LIST_BYTE:
+                return toListByte(value);
+
+            case LIST_STRING:
+                return toListString(value);
+
+            case LIST_BOOLEAN:
+                return toListBoolean(value);
+
+            default:
+                LOG.error(NO_DATA_TYPE_MESSAGE);
+                return null;
+        }
+    }
+
+    private static List<Integer> toListInteger(String value) {
+        List<Integer> integers = new ArrayList<>();
+        if (value == null) {
+            return integers;
+        }
+
+        List<String> strings = toList(value);
+        for (String string : strings) {
+            integers.add(Integer.parseInt(string));
+        }
+
+        return integers;
+    }
+
+    private static List<BigInteger> toListBigInteger(String value) {
+        List<BigInteger> bigIntegers = new ArrayList<>();
+        if (value == null) {
+            return bigIntegers;
+        }
+
+        List<String> strings = toList(value);
+        for (String string : strings) {
+            bigIntegers.add(new BigInteger(string));
+        }
+
+        return bigIntegers;
+    }
+
+    private static List<Double> toListDouble(String value) {
+        List<Double> doubles = new ArrayList<>();
+        if (value == null) {
+            return doubles;
+        }
+
+        List<String> strings = toList(value);
+        for (String string : strings) {
+            doubles.add(Double.parseDouble(string));
+        }
+
+        return doubles;
+    }
+
+    private static List<Byte> toListByte(String value) {
+        List<Byte> bytes = new ArrayList<>();
+        if (value == null) {
+            return bytes;
+        }
+
+        List<String> strings = toList(value);
+        for (String string : strings) {
+            bytes.add(Byte.parseByte(string));
+        }
+
+        return bytes;
+    }
+
+    private static List<String> toListString(String value) {
+        List<String> strings = new ArrayList<>();
+        if (value == null) {
+            return strings;
+        }
+
+        strings = toList(value);
+        return strings;
+    }
+
+    private static List<Boolean> toListBoolean(String value) {
+        List<Boolean> booleans = new ArrayList<>();
+        if (value == null) {
+            return booleans;
+        }
+
+        List<String> strings = toList(value);
+        for (String string : strings) {
+            booleans.add(Boolean.parseBoolean(string));
+        }
+
+        return booleans;
+    }
+
+    private static List<String> toList(String value) {
+        return Arrays.asList(value.substring(1, value.length() - 1).trim().split(","));
+    }
+}
