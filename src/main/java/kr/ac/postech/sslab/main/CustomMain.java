9be7106ae46c5a74c30f4789131e8143127e2319
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-07 14:58:01 +0900

    update CustomMain because of cognitive complexity

--- src/main/java/kr/ac/postech/sslab/main/CustomMain.java
+++ src/main/java/kr/ac/postech/sslab/main/CustomMain.java
@@ -1,5 +1,6 @@
 package kr.ac.postech.sslab.main;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.ac.postech.sslab.extension.*;
@@ -7,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.*;
 import static kr.ac.postech.sslab.constant.Message.ARG_MESSAGE;
@@ -25,212 +27,64 @@ public class CustomMain extends Main {
             String response;
 
             switch (func) {
-                case "balanceOf": {
+                case "balanceOf":
                     if (args.size() == 1) {
                         return super.invoke(stub);
                     }
-
-                    if (args.size() != 2 || isNullOrEmpty(args.get(0))
-                    || isNullOrEmpty(args.get(1))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE + "1 or 2"));
-                    }
-
-                    String owner = args.get(0);
-                    String type = args.get(1);
-                    BigInteger balance = EERC721.balanceOf(stub, owner, type);
-                    response = balance.toString();
+                    response = balanceOf(stub, args);
                     break;
-                }
 
-                case "tokenIdsOf": {
-                    List<BigInteger> tokenIds;
-                    if (args.size() == 1) {
-                        if (isNullOrEmpty(args.get(0))) {
-                            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
-                        }
-
-                        String owner = args.get(0);
-                        tokenIds = EERC721.tokenIdsOf(stub, owner);
-                    }
-                    else if (args.size() == 2) {
-                        if (isNullOrEmpty(args.get(0)) || isNullOrEmpty(args.get(1))) {
-                            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
-                        }
-
-                        String owner = args.get(0);
-                        String type = args.get(1);
-                        tokenIds = EERC721.tokenIdsOf(stub, owner, type);
-                    }
-                    else {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE + "1 or 2"));
-                    }
-
-                    response = tokenIds.toString();
+                case "tokenIdsOf":
+                    response = tokenIdsOf(stub, args);
                     break;
-                }
 
-                case "divide": {
-                    if (args.size() != 4 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2)) || isNullOrEmpty(args.get(3))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "4"));
-                    }
-
-                    BigInteger tokenId = new BigInteger(args.get(0));
-
-                    List<String> newIdsStr = Arrays.asList(args.get(1)
-                            .substring(1, args.get(1).length() - 1).split(", "));
-                    List<BigInteger> newIds = new ArrayList<>();
-                    for (String newIdStr : newIdsStr) {
-                        BigInteger newId = new BigInteger(newIdStr);
-                        newIds.add(newId);
-                    }
-
-                    List<String> values = Arrays.asList(args.get(2)
-                            .substring(1, args.get(2).length() - 1).split(", "));
-
-                    String index = args.get(3);
-                    boolean result = EERC721.divide(stub, tokenId, newIds, values, index);
-                    response = Boolean.toString(result);
+                case "divide":
+                    response = divide(stub, args);
                     break;
-                }
 
-                case "deactivate": {
-                    if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
-                    }
-
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    boolean result = EERC721.deactivate(stub, tokenId);
-                    response = Boolean.toString(result);
+                case "deactivate":
+                    response = deactivate(stub, args);
                     break;
-                }
 
-                case "query": {
-                    if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
-                    }
-
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String query = EERC721.query(stub, tokenId);
-                    response = query;
+                case "query":
+                    response = query(stub, args);
                     break;
-                }
 
-                case "queryHistory": {
-                    if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
-                    }
-
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    List<String> histories = EERC721.queryHistory(stub, tokenId);
-                    response = histories.toString();
+                case "queryHistory":
+                    response = queryHistory(stub, args);
                     break;
-                }
 
-                case "mint": {
-                    if (args.size() == 2) {
-                        return super.invoke(stub);
-                    }
-
-                    if (args.size() != 5 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))
-                            || isNullOrEmpty(args.get(3)) || isNullOrEmpty(args.get(4))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "5"));
-                    }
-
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String type = args.get(1);
-                    String owner = args.get(2);
-                    Map<String, Object> xattr =
-                            mapper.readValue(args.get(3), new TypeReference<HashMap<String, Object>>(){});
-                    Map<String, String> uri =
-                            mapper.readValue(args.get(4), new TypeReference<HashMap<String, String>>(){});
-
-                    LOG.info("CustomMain::invoke:: call XNFT.mint");
-                    boolean result = XNFT.mint(stub, tokenId, type, owner, xattr, uri);
-                    response = Boolean.toString(result);
+                case "mint":
+                    response = mint(stub, args);
                     break;
-                }
 
-                case "setURI": {
-                    if (args.size() != 3 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
-                    }
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String index = args.get(1);
-                    String value = args.get(2);
-                    boolean result = XNFT.setURI(stub, tokenId, index, value);
-                    response = Boolean.toString(result);
+                case "setURI":
+                    response = setURI(stub, args);
                     break;
-                }
 
-                case  "getURI": {
-                    if (args.size() != 2 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
-                    }
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String index = args.get(1);
-                    String value = XNFT.getURI(stub, tokenId, index);
-                    response = value;
+                case  "getURI":
+                    response = getURI(stub, args);
                     break;
-                }
 
-                case "setXAttr": {
-                    if (args.size() != 3 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
-                    }
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String index = args.get(1);
-                    String value = args.get(2);
-                    boolean result = XNFT.setXAttr(stub, tokenId, index, value);
-                    response = Boolean.toString(result);
+                case "setXAttr":
+                    response = setXAttr(stub, args);
                     break;
-                }
 
-                case "getXAttr": {
-                    if (args.size() != 2 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
-                    }
-                    BigInteger tokenId = new BigInteger(args.get(0));
-                    String index = args.get(1);
-                    Object value = XNFT.getXAttr(stub, tokenId, index);
-                    response = (String) value;
+                case "getXAttr":
+                    response = getXAttr(stub, args);
                     break;
-                }
 
-                case "registerTokenType": {
-                    if (args.size() != 2 || isNullOrEmpty(args.get(0))
-                            || isNullOrEmpty(args.get(1))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
-                    }
-
-                    String type = args.get(0);
-                    String json = args.get(1);
-                    boolean result = XType.registerTokenType(stub, type, json);
-                    response = Boolean.toString(result);
+                case "registerTokenType":
+                    response = registerTokenType(stub, args);
                     break;
-                }
 
-                case "tokenTypesOf": {
-                    List<String> tokenTypes = XType.tokenTypesOf();
-                    response = tokenTypes.toString();
+                case "tokenTypesOf":
+                    response = tokenTypesOf();
                     break;
-                }
 
-                case "getTokenType": {
-                    if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
-                        throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
-                    }
-
-                    String type = args.get(0);
-                    Map<String, List<String>> map = XType.getTokenType(type);
-                    response = mapper.writeValueAsString(map);
+                case "getTokenType":
+                    response = getTokenType(args);
                     break;
-                }
 
                 default:
                     return super.invoke(stub);
@@ -241,6 +95,198 @@ public class CustomMain extends Main {
             LOG.error("CustomMain::invoke:: invoke error");
             return newErrorResponse(e.getMessage());
         }
+    }
+
+    private String balanceOf(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 2 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE + "1 or 2"));
+        }
+
+        String owner = args.get(0);
+        String type = args.get(1);
+
+        return EERC721.balanceOf(stub, owner, type).toString();
+    }
+
+    private String tokenIdsOf(ChaincodeStub stub, List<String> args) throws IOException {
+        List<BigInteger> tokenIds;
+
+        if (args.size() == 1) {
+            if (isNullOrEmpty(args.get(0))) {
+                throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
+            }
+
+            String owner = args.get(0);
+            tokenIds = EERC721.tokenIdsOf(stub, owner);
+        }
+        else if (args.size() == 2) {
+            if (isNullOrEmpty(args.get(0)) || isNullOrEmpty(args.get(1))) {
+                throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
+            }
+
+            String owner = args.get(0);
+            String type = args.get(1);
+            tokenIds = EERC721.tokenIdsOf(stub, owner, type);
+        }
+        else {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE + "1 or 2"));
+        }
+
+        return tokenIds.toString();
+    }
+
+    private String divide(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 4
+                || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1))
+                || isNullOrEmpty(args.get(2))
+                || isNullOrEmpty(args.get(3))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "4"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+
+        List<String> newIdsStr = strToList(args.get(1));
+        List<BigInteger> newIds = new ArrayList<>();
+        for (String newIdStr : newIdsStr) {
+            BigInteger newId = new BigInteger(newIdStr);
+            newIds.add(newId);
+        }
+
+        List<String> values = strToList(args.get(2));
+
+        String index = args.get(3);
+
+        return Boolean.toString(EERC721.divide(stub, tokenId, newIds, values, index));
+    }
+
+    private String deactivate(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+
+        return Boolean.toString(EERC721.deactivate(stub, tokenId));
+    }
+
+    private String query(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+
+        return EERC721.query(stub, tokenId);
+    }
+
+    private String queryHistory(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+
+        return EERC721.queryHistory(stub, tokenId).toString();
+    }
+
+    private String mint(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 5 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))
+                || isNullOrEmpty(args.get(3)) || isNullOrEmpty(args.get(4))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "5"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+        String type = args.get(1);
+        String owner = args.get(2);
+        Map<String, Object> xattr =
+                mapper.readValue(args.get(3), new TypeReference<HashMap<String, Object>>(){});
+        Map<String, String> uri =
+                mapper.readValue(args.get(4), new TypeReference<HashMap<String, String>>(){});
+
+        return Boolean.toString(XNFT.mint(stub, tokenId, type, owner, xattr, uri));
+    }
+
+    private String setURI(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 3 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+        String index = args.get(1);
+        String value = args.get(2);
+
+        return Boolean.toString(XNFT.setURI(stub, tokenId, index, value));
+    }
+
+    private String getURI(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 2 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
+        }
+        BigInteger tokenId = new BigInteger(args.get(0));
+        String index = args.get(1);
+        return XNFT.getURI(stub, tokenId, index);
+    }
+
+    private String setXAttr(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 3 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+        String index = args.get(1);
+        String value = args.get(2);
+
+        return Boolean.toString(XNFT.setXAttr(stub, tokenId, index, value));
+    }
+
+    private String getXAttr(ChaincodeStub stub, List<String> args)  throws IOException {
+        if (args.size() != 2 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
+        }
+
+        BigInteger tokenId = new BigInteger(args.get(0));
+        String index = args.get(1);
+
+        return XNFT.getXAttr(stub, tokenId, index);
+    }
+
+    private String registerTokenType(ChaincodeStub stub, List<String> args) throws IOException {
+        if (args.size() != 2 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "2"));
+        }
+
+        String type = args.get(0);
+        String json = args.get(1);
+
+        return Boolean.toString(XType.registerTokenType(stub, type, json));
+    }
+
+    private String tokenTypesOf() {
+        List<String> tokenTypes = XType.tokenTypesOf();
+        return tokenTypes.toString();
+    }
+
+    private String getTokenType(List<String> args) throws JsonProcessingException {
+        if (args.size() != 1 || isNullOrEmpty(args.get(0))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "1"));
+        }
+
+        String type = args.get(0);
+        Map<String, List<String>> map = XType.getTokenType(type);
+
+        return mapper.writeValueAsString(map);
+    }
+
+    private List<String> strToList(String str) {
+        return Arrays.asList(str.substring(1, str.length() - 1).trim().split(","));
     }
 
     public static void main(String[] args) {
