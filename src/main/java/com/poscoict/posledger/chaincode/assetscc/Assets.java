d7d23a5cb6a142b80b859ff15c6beca01cf6bfa9
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-08 17:38:23 +0900

    root class to upload on PosLedger Manager

---
+++ src/main/java/com/poscoict/posledger/chaincode/assetscc/Assets.java
@@ -0,0 +1,9 @@
+package com.poscoict.posledger.chaincode.assetscc;
+
+import com.poscoict.posledger.chaincode.assetscc.main.CustomMain;
+
+public class Assets extends CustomMain {
+    public static void main(String[] args) {
+        new Assets().start(args);
+    }
+}
