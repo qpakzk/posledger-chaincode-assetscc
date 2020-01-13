eeafae14954d553b30a3433a28c674591656200b
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-13 20:36:43 +0900

    rename a param to match regular expression

--- src/main/java/com/poscoict/posledger/chaincode/assetscc/extension/XType.java
+++ src/main/java/com/poscoict/posledger/chaincode/assetscc/extension/XType.java
@@ -15,14 +15,14 @@ import static com.poscoict.posledger.chaincode.assetscc.constant.Key.*;
 public class XType extends CustomChaincodeBase {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static boolean enroll(ChaincodeStub stub, String _admin, String type, String json) throws IOException {
+    public static boolean enroll(ChaincodeStub stub, String admin, String type, String json) throws IOException {
         Map<String, List<String>> attributes = objectMapper.readValue(json, new TypeReference<HashMap<String, List<String>>>() {});
 
         if (!attributes.containsKey(ADMIN_KEY)) {
             return false;
         }
 
-        if (!_admin.equals(attributes.get(ADMIN_KEY).get(1))) {
+        if (!admin.equals(attributes.get(ADMIN_KEY).get(1))) {
             return false;
         }
 
@@ -48,11 +48,10 @@ public class XType extends CustomChaincodeBase {
         return manager.addTokenType(stub, type, attributes);
     }
 
-    public static boolean drop(ChaincodeStub stub, String _admin, String tokenType) throws IOException {
+    public static boolean drop(ChaincodeStub stub, String admin, String tokenType) throws IOException {
         TokenTypeManager manager = TokenTypeManager.read(stub);
 
-        String admin = manager.getAdmin(tokenType);
-        if (!_admin.equals(admin)) {
+        if (!admin.equals(manager.getAdmin(tokenType))) {
             return false;
         }
 
@@ -64,11 +63,10 @@ public class XType extends CustomChaincodeBase {
         return new ArrayList<>(manager.getTokenTypes().keySet());
     }
 
-    public static boolean update(ChaincodeStub stub, String _admin, String tokenType, Map<String, List<String>> attributes) throws IOException {
+    public static boolean update(ChaincodeStub stub, String admin, String tokenType, Map<String, List<String>> attributes) throws IOException {
         TokenTypeManager manager = TokenTypeManager.read(stub);
 
-        String admin = manager.getAdmin(tokenType);
-        if (!_admin.equals(admin)) {
+        if (!admin.equals(manager.getAdmin(tokenType))) {
             return false;
         }
 
@@ -103,33 +101,30 @@ public class XType extends CustomChaincodeBase {
         return manager.getTokenType(tokenType);
     }
 
-    public static boolean enrollAttribute(ChaincodeStub stub, String _admin, String tokenType, String attribute, String dataType, String initialValue) throws IOException {
+    public static boolean enrollAttribute(ChaincodeStub stub, String admin, String tokenType, String attribute, String dataType, String initialValue) throws IOException {
         TokenTypeManager manager = TokenTypeManager.read(stub);
 
-        String admin = manager.getAdmin(tokenType);
-        if (!_admin.equals(admin)) {
+        if (!admin.equals(manager.getAdmin(tokenType))) {
             return false;
         }
 
         return manager.addAttributeOfTokenType(stub, tokenType, attribute, dataType, initialValue);
     }
 
-    public static boolean dropAttribute(ChaincodeStub stub, String _admin, String tokenType, String attribute) throws IOException {
+    public static boolean dropAttribute(ChaincodeStub stub, String admin, String tokenType, String attribute) throws IOException {
         TokenTypeManager manager = TokenTypeManager.read(stub);
 
-        String admin = manager.getAdmin(tokenType);
-        if (!_admin.equals(admin)) {
+        if (!admin.equals(manager.getAdmin(tokenType))) {
             return false;
         }
 
         return manager.removeAttributeOfTokenType(stub, tokenType, attribute);
     }
 
-    public static boolean updateAttribute(ChaincodeStub stub, String _admin, String tokenType, String attribute, List<String> pair) throws IOException {
+    public static boolean updateAttribute(ChaincodeStub stub, String admin, String tokenType, String attribute, List<String> pair) throws IOException {
         TokenTypeManager manager = TokenTypeManager.read(stub);
 
-        String admin = manager.getAdmin(tokenType);
-        if (!_admin.equals(admin)) {
+        if (!admin.equals(manager.getAdmin(tokenType))) {
             return false;
         }
 
